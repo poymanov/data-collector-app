142c67f679b64a820966189beaf615f0d0cde35e
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-23 21:25:44 +0300

    Add base views and controllers

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('/success', methods=['POST'])
+def success():
+	email = request.form['email']
+	height = request.form['height']
+	return render_template('success.html')	
