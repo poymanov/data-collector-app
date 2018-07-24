644c1986094e9da096a973b869256d96f2e06260
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-24 21:25:31 +0300

    Add extra info to email message

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, request, redirect, url_for, flash
 from app.forms import DataForm
 from app.models import Data
 from app.email import send_email
+from sqlalchemy.sql import func
 
 @app.route('/')
 def home():		
@@ -24,9 +25,13 @@ def success():
 		data = Data(email=email, height=height)
 		db.session.add(data)
 		db.session.commit()
+		
+		average_height = db.session.query(func.avg(Data.height)).scalar()
+		average_height = round(average_height, 1)
+		people_count = db.session.query(Data).count()
 
-		text_body = render_template('/email/height_mail.txt', height=height)
-		html_body = render_template('/email/height_mail.html', height=height)
+		text_body = render_template('/email/height_mail.txt', height=height, average_height=average_height, people_count=people_count)
+		html_body = render_template('/email/height_mail.html', height=height, average_height=average_height, people_count=people_count)
 		send_email(subject="Height data", recipients=[email], text_body=text_body, html_body=html_body)
 
 		return render_template('success.html')	
